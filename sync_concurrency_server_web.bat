a86684c844934f24a9bd13a158a1243951900239
JaydenOK <603480498@qq.com>
2023-03-19 20:42:11 +0800

    Context

--- sync_concurrency_server_web.bat
+++ sync_concurrency_server_web.bat
@@ -3,7 +3,7 @@
 ::::::::::::::  stop
 echo Start Sync ...
 
-D:/www/rsync/cwRsync_5.4/rsync.exe -avzP  --port=873 --delete --no-super --password-file=/cygdrive/D/www/rsync/cwRsync_5.4/pass.txt --exclude=logs/* --exclude=.git/ --exclude=.idea/ /cygdrive/D/www/sw-www/mysql-pool-concurrency-server-web/ root@192.168.92.208::mysql_pool_concurrency_server_web
+D:/www/rsync/cwRsync_5.4/rsync.exe -avzP  --port=873 --delete --no-super --password-file=/cygdrive/D/www/rsync/cwRsync_5.4/pass.txt --exclude=logs/* --exclude=.git/ --exclude=.idea/ /cygdrive/D/www/sw-www/swoole-mysqlpool-concurrency-server-web/ root@192.168.92.208::mysql_pool_concurrency_server_web
 
 echo Success...
 :: 延时
